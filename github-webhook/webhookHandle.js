d749313ebcb1bae0950fccb2f60b81d57121e5cc
chad.zha <yaping.zha@medeen.com>
2017-09-30 17:43:44 +0800

    cehck hook

--- github-webhook/webhookHandle.js
+++ github-webhook/webhookHandle.js
@@ -3,6 +3,25 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler');
 var handler = createHandler({ path: '/reviewpushcode', secret: 'zhablog' })
+var exec = require('child_process').exec;
+
+function run_cmd() {
+    exec('git pull origin master',function(err,stdout,stderr){
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log(stdout);
+        exec('pm2 restart all',function(err,stdout,stderr){
+            if(err){
+                consol.log(err)
+                return;
+            }
+            console.log(stdout);
+        });
+    })
+}
+
  
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -21,6 +40,7 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+    run_cmd();
 })
  
 handler.on('issues', function (event) {
@@ -30,35 +50,3 @@ handler.on('issues', function (event) {
     event.payload.issue.number,
     event.payload.issue.title)
 })
-
-// var http = require('http');
-
-// //var handler = ({ path: '/reviewpushcode', secret: 'zhablog' });
-// var exec = require('child_process').exec;
-
-// function run_cmd() {
-//     exec('git pull origin master',function(err,stdout,stderr){
-//         if(err){
-//             console.log(err);
-//             return;
-//         }
-//         console.log(stdout);
-//         exec('pm2 restart all',function(err,stdout,stderr){
-//             if(err){
-//                 consol.log(err)
-//                 return;
-//             }
-//             console.log(stdout);
-//         });
-//     })
-// }
- 
-// http.createServer(function (req, res) {
-//     if(req.url === '/reviewpushcode' && req.method === ''){
-//         console.log(req);
-//         res.write('ok');
-//         res.end();
-//     }
-// }).listen(3389,function(){
-//     console.log('webhook is running on 3389...');
-// })
