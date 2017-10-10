b721f832d98f0964ccb8ebc1e9d1d7e4ceb199f3
chad.zha <yaping.zha@medeen.com>
2017-10-10 15:48:53 +0800

    fx

--- github-webhook/webhookHandle.js
+++ github-webhook/webhookHandle.js
@@ -4,48 +4,57 @@ var handler = createHandler({ path: '/reviewpushcode', secret: 'zhablog' })
 var exec = require('child_process').exec;
 
 function run_cmd() {
-    exec('git pull origin master',function(err,stdout,stderr){
-        if(err){
+    exec('git pull origin master', function (err, stdout, stderr) {//拉取主分支代码
+        if (err) {
             console.log(err);
             return;
         }
         console.log(stdout);
-        exec('pm2 restart www',function(err,stdout,stderr){
-            if(err){
+        exec('webpack', function (err, stdout, stderr) {//进行webpack编译
+            if (err) {
                 consol.log(err)
                 return;
             }
             console.log(stdout);
-        });
+
+            exec('pm2 restart www', function (err, stdout, stderr) {//重启网站服务器
+                if (err) {
+                    consol.log(err)
+                    return;
+                }
+                console.log(stdout);
+            });
+        })
+
     })
 }
 
- 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
 
-}).listen(3389,function(){
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404
+        res.end('no such location')
+    })
+
+}).listen(3389, function () {
     console.log('webhook is running on 3389...');
 })
- 
+
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+    console.error('Error:', err.message)
 })
- 
+
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
     run_cmd();
 })
- 
+
 handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
+    console.log('Received an issue event for %s action=%s: #%d %s',
+        event.payload.repository.name,
+        event.payload.action,
+        event.payload.issue.number,
+        event.payload.issue.title)
 })
